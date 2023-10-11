1c3e10cdee0d0068934894c566dc3bcdc04bcfa8
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-10-11 21:46:45 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 
-import Footer from "./Components/Footer";
-import Contact from "./Components/Contact";
+import Footer from "./Pages/Footer";
+import Contact from "./Pages/Contact";
 import Hero from "./Components/Hero";
+import About from "./Pages/About";
+
+
+
 function App() {
   return (
     <div className="app">
@@ -11,9 +15,8 @@ function App() {
       <Navbar></Navbar>     
       <Routes>
         <Route path="/" element={<Hero></Hero>}></Route>
-        <Route path="/contact" element={<Contact></Contact>}>
-        {/* <Route path="/about" element={<About></About>}></Route> */}
-        </Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
       </Routes>
       <Footer/>
     
