be6d09e87a980098398c359584ad51d0aa887f8f
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-10-13 01:17:09 +0530

    updated Whyus

--- src/Components/WhyUs.js
+++ src/Components/WhyUs.js
@@ -57,13 +57,7 @@ const WhyUs = () => {
 
   return (
     
-    <motion.div  ref={ref}
-    style={
-      {
-        scale:scaleProgess,
-        opacity:opacityProgess,
-      }
-    } className="bg-gray-100 py-10 md:py-20 px-4">
+    <div className="bg-gray-100 py-10  px-4">
       <h1
         onMouseEnter={() => {
           textEnter();
@@ -73,7 +67,7 @@ const WhyUs = () => {
           textLeave();
           setHeadingHovered(false);
         }}
-        className={`text-[40px] text-center font-bold md:text-center md:text-[55px] md:font-bold mt-4 md:mt-0 md:-mb-[80px]  mb-6 ${
+        className={`heading text-[#164154] text-[40px]  text-center font-bold md:text-center md:text-[55px] md:font-bold mt-4 md:mt-0 md:mb-[60px]  mb-6 ${
           headingHovered ? 'text-why-hovered' : '' 
         }`}
       >
@@ -97,20 +91,20 @@ const WhyUs = () => {
             }} className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:translate-x-0 md:translate-y-0 md:top-[15%] md:left-[18%] md:w-[65%] md:rounded-xl md:h-auto md:bg-[#fff] md:p-8">
           <div className="flex gap-[70px]">
             <img
-              className="hidden lg:block md:mt-0 md:w-[350px] md:h-[300px] md:flex items-center  md:rounded-xl"
+              className="hidden lg:block md:mt-[10px] md:w-[350px] md:h-[300px]  md:flex items-center  md:rounded-xl"
               src={why}
               alt="Why Us Image"
             />
 
             <div className="mt-4 w-[300px] h-[100px] md:w-auto md:h-[320px]  overflow-auto md:mt-0 max-w-full md:max-w-xl">
-              <p className="text-base text-white md:text-[19px] lg:text-[19px] md:text-justify md:leading-[30px] md:text-[#000000]">
+              <p className="text-base text-white md:text-[24px] md:text-justify leading-[30px] md:text-[#164154]">
               If you feel overwhelmed by the demands of city life, your responsibilities, and the stresses of your job, it may be time to take a step back and prioritize your well-being. You deserve an opportunity to reconnect with your passions and gain a fresh perspective. Allow us to guide you on a journey into the heart of nature, where time slows down, and you can focus on self-care. Let go of all that is holding you back from the things you love and discover your inner wisdom. Become "The Sage" and live the life you truly deserve.
               </p>
             </div>
           </div>
         </motion.div>
       </div>
-    </motion.div>
+    </div>
   
   );
 };
