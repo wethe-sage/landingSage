241c22469c46e1149534b21db000c56f847ed65b
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-10-13 01:17:01 +0530

    updated TravelChanges

--- src/Components/TravelChange.js
+++ src/Components/TravelChange.js
@@ -68,7 +68,7 @@ const TravelChange = () => {
   
   return (
     <div className='bg-gray-100 py-10 px-4'>
-      <h1 onMouseEnter={textEnter} onMouseLeave={textLeave}   className="text-[40px] text-center font-bold md:text-center text-[#164154]  md:text-[55px] md:font-bold mt-4 md:mt-0 md:-mb-[80px] mb-6">
+      <h1 onMouseEnter={textEnter} onMouseLeave={textLeave}   className=" heading text-[40px] text-center font-bold md:text-center text-[#164154] md:text-[55px] md:font-bold mt-4 md:mt-[20px] md:pb-[10px] mb-6">
         How Travel Changes People?
       </h1>
       {window.innerWidth > 768 && (
@@ -98,7 +98,7 @@ const TravelChange = () => {
             />
           
           <div className='mt-4 w-[300px] h-[100px] md:w-auto md:h-[340px] overflow-auto md:mt-0 max-w-full md:max-w-xl '>
-          <p className='text-base text-white md:text-[24px] md:text-justify leading-[30px] md:text-[#000000]'>
+          <p className='text-base text-white md:text-[24px] md:text-justify leading-[30px] md:text-[#164154]'>
           Travel has the power to transform people in profound and meaningful ways. It exposes us to new cultures, challenges us, and helps us grow. Through travel, we gain a deeper appreciation for the diversity that exists around us and learn to be more open-minded and adaptable. We discover passions and interests we never knew we had and find a sense of purpose and direction in life. Travel changes us, and it's a journey that can never be forgotten.
           </p>
           </div>
