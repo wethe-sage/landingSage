ed965641a7fa945436ea916f0483e51828d9f770
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-10-14 00:13:35 +0530

    updated ServicesCarrd

--- src/Components/ServiceCard.js
+++ src/Components/ServiceCard.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ServiceCard = (props) => {
   return (
-    <div>
-      <div className='hover:shadow-xl  flex select-none rounded-xl  p-[30px] flex-col justify-center items-center bg-white w-[280px] h-[270px] max-h-[250px]'>
+    <div className='select-none'>
+      <div className='hover:shadow-xl  flex select-none rounded-xl  p-[20px] flex-col justify-center items-center bg-white w-[280px] h-[300px] max-h-[350px]'>
         <h1 className='text-[70px] text-[#bcad3c] mt-[5px]'>{props.image}</h1>
         <h1 className='heading font-semibold text-2xl mt-[20px]  text-blue-950'>{props.name}</h1>
         <p className='text-[#164154] text-sm p-[10px] text-justify'>{props.description}</p>
