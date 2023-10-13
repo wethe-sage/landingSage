bea1aa016a18b085bccbc42a46eb10d52d6eba9b
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-10-14 00:14:14 +0530

    updated Contact

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -10,29 +10,23 @@
   
           <div className='max-w-[1000px] -mt-[50px]  p-4 flex flex-col justify-center w-full h-full'>
               
-              <p className='text-[45px] font-bold inline border-b-4 border-[#000000] text-black pb-[1px]'>Get in Touch</p>
-              <p className='text-black py-2 text-start text-[17px]'>
-                  Submit the form below or send us an mail - explore.sage@gmail.com
+              <p className='text-[45px] hea font-bold inline border-b-4 border-[#000000] text-[#164154] pb-[1px]'>Reach our SAGES</p>
+              <p className='subHeading text-[#164154] py-2 text-start text-[17px]'>
+                  Submit the form below or send us an mail - info@wethesage.com
               </p>
-          </div>
-          
-          <input className='bg-[#d3d3d3] p-2 my-2 rounded-lg' type='text' placeholder='Name' name='name' >
+          </div>          
+          <input className='bg-[#d3d3d3] text-[#164154] p-2 my-2 rounded-lg' type='text' placeholder='Name' name='name' >
           </input>
-          <input type='email' placeholder='Email' name='email'className=' my-2 p-2 bg-[#d3d3d3]  rounded-lg' >
+          <input type='email' placeholder='Email' name='email'className=' text-[#164154] my-2 p-2 bg-[#d3d3d3]  rounded-lg' >
           </input>
-          <input type='text' placeholder='Subject' name='subject'className='my-2 p-2 rounded-lg bg-[#d3d3d3] ' >
+          <input type='text' placeholder='Subject' name='subject'className=' text-[#164154] my-2 p-2 rounded-lg bg-[#d3d3d3] ' >
           </input>
-          <textarea className=' bg-[#d3d3d3]  rounded-lg p-2 my-1' name='message' cols='30' rows="10" placeholder='Message'>
-          </textarea>
-          
-          <button className='text-black border-2 hover:bg-[#d3d3d3] hover:border-black px-4 py-2 my-8 mx-auto flex items-center rounded-md'>Send Message</button>
-          
+          <textarea className=' text-[#164154] bg-[#d3d3d3]  rounded-lg p-2 my-1' name='message' cols='30' rows="10" placeholder='Message'>
+          </textarea>          
+          <button className='text-[#164154] hover:text-[#fff] border-2 hover:bg-[#bcad3c] border-[#bcad3c] hover:border-[#164154] px-4 py-2 my-8 mx-auto flex items-center rounded-md'>Send Message</button>
       </form>
       </div>
-      
     </div>
-          
-        
       )
     }
     
